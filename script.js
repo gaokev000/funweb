8e4014f0c9e3b7e11e188d413efc27e259e35fda
gaokev000 <gaokev000@mysbisd.org>
2024-08-24 20:28:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const buttonHeight = 50;
 const buttonWidth = 120;
-const maxWidth = window.innerWidth - buttonWidth;
-const maxHeight = window.innerHeight - buttonHeight;
+const maxWidth = window.innerWidth/2 - buttonWidth-10;
+const maxHeight = window.innerHeight/2 - buttonHeight-10;
 
 window.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('button2');
-    button.addEventListener('click', () => alert('Wrong'));
-    button.addEventListener('mouseover', () =>{
+    button.addEventListener('click', () => {
         button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
         button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
     });
